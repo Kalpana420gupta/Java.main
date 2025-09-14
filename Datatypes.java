f199348fba9ec7bb6c33f69b19ded367d39d7781
Kalpana Gupta <kalpana012210gupta@gmail.com>
2025-09-14 19:16:38 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,9 @@
+public class Datatypes {
+    public static void main(String arg[]){
+        byte a=5;
+        System.out.println(a);
+        char ch='b';
+        System.out.println(ch);
+        
+    }
+}
