fa755b32f035ba81786641d68249652079c4e72c
Kalpana Gupta <kalpana012210gupta@gmail.com>
2025-09-14 19:17:54 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String arg[]){
+        int a=20;
+        int b=10;
+        System.out.println(a);
+        System.out.println(b);
+        String name="Kalpana gupta";
+        System.out.println(name);
+
+        a=15;
+        System.out.println(a);
+    }
+    
+}
